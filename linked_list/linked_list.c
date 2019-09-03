3da8140b35956c45fb607c6dc7453d982deb9e5c
MeowDada <chang.hungjui1251@gmail.com>
2019-09-03 15:00:16 +0800

    Add linked_list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -106,3 +106,120 @@ void linked_list_dump(linked_list_t _node, void (*printFunc)(any_t))
     list_t *node = (list_t *)_node;
     (*printFunc)(node->data);
 }
+
+/* Operation for list */
+int linked_list_get_total_length(linked_list_t _node)
+{
+    list_t *node = (list_t *)_node;
+    if (!node)
+        return 0;
+    int cnt = 1;
+
+    list_t *prev = node->prev;
+    list_t *next = node->next;
+    while (prev) {
+        prev = prev->prev;
+        cnt++;
+    }
+    while (next) {
+        next = next->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+void linked_list_destroy_list(linked_list_t _node)
+{
+    if (!_node)
+        return;
+    list_t *node = (list_t *)_node;
+    list_t *next = node->next;
+    list_t *prev = node->prev;
+    linked_list_destroy(prev);
+    linked_list_destroy(next);
+}
+
+void linked_list_iterate(linked_list_t _node, PFany fptr, any_t args)
+{
+    if (!_node || !fptr)
+        return;
+    
+    list_t *node = (list_t *)_node;
+    while (node) {
+        (*fptr)(node->data, args);
+        node = node->next;
+    }
+}
+
+linked_list_t linked_list_get_node(linked_list_t _node, int idx)
+{
+    if (!_node || idx < 0)
+        return NULL;
+    
+    list_t *node = (list_t *)_node;
+    for (int i = 0 ; i < idx; i++) {
+        if (node->next)
+            node = node->next;
+        else
+            return NULL;
+    }
+    return node;
+}
+
+void linked_list_dump_list(linked_list_t _node, void (*printFunc)(any_t))
+{
+    if (!_node || !printFunc)
+        return;
+    
+    list_t *node = (list_t *)_node;
+    while (node) {
+        printFunc(node->data);
+        node = node->next;
+    }
+}
+
+linked_list_t linked_list_insert_tail(linked_list_t _list, linked_list_t _node)
+{
+    if (!_list || !_node)
+        return;
+    
+    list_t *list = (list_t *)_list;
+    list_t *node = (list_t *)_node;
+    list_t *list_head = list;
+
+    while (list->next)
+        list = list->next;
+    
+    while (list_head->prev)
+        list_head = list_head->prev;
+    
+    list->next = node;
+    node->prev = list;
+
+    return list_head;
+}
+
+linked_list_t linked_list_insert_front(linked_list_t _list, linked_list_t _node)
+{
+    if (!_list || !_node)
+        return;
+    
+    list_t *list = (list_t *)_list;
+    list_t *node = (list_t *)_node;
+    list_t *node_tail = node;
+    list_t *node_head = node;
+
+    while (list->prev)
+        list = list->prev;
+
+    while (node_tail->next)
+        node_tail = node_tail->next;
+    
+    while (node_head->prev)
+        node_head = node_head->prev;
+    
+    list->prev = node_tail;
+    node_tail->next = list;
+
+    return node_head;
+}

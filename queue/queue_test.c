acc7e69ecca4cc29b37889a4c412560b741477af
MeowDada <chang.hungjui1251@gmail.com>
2019-09-04 10:32:35 +0800

    Add test case for queue_test

--- queue/queue_test.c
+++ queue/queue_test.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int main(int argc, char **argv)
+static void queue_basic_test(int type)
 {
-    int capacity = 10;
+    int capacity = 5;
     size_t sizeof_int = sizeof(int);
     queue_t q = queue_create(QUEUE_IMPL_BY_ARRAY, &capacity, &sizeof_int);
 
     int arr[] = {0,1,2,3,4,5,6,7,8,9};
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 7; i++) {
         queue_enqueue(q, &arr[i]);
     }
 
@@ -23,6 +23,13 @@ int main(int argc, char **argv)
     printf("\n");
 
     queue_destroy(q);
+}
+
+int main(int argc, char **argv)
+{
+    queue_basic_test(QUEUE_IMPL_BY_ARRAY);
+    queue_basic_test(QUEUE_IMPL_BY_VECTOR);
+    queue_basic_test(QUEUE_IMPL_BY_LINKED_LIST);
 
     return 0;
 }

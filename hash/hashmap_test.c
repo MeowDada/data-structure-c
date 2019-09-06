838950c351fd40d2cd8fbfc9bc55335a4170e53a
MeowDada <chang.hungjui1251@gmail.com>
2019-09-06 11:41:22 +0800

    Add more test

--- hash/hashmap_test.c
+++ hash/hashmap_test.c
@@ -14,9 +14,10 @@ int main(int argc, char **argv)
 {
     hashmap_t map = hashmap_create(HASHMAP_CHAINING, hash_int, hash_int_equal);
 
-    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < 32; i++)
         hashmap_insert(map, &arr[i], &arr[i]);
     hashmap_dump(map, print_int);
 

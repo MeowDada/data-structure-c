e5810b7f5c94b381b39cd5be9fff50f780bd10b8
MeowDada <chang.hungjui1251@gmail.com>
2019-09-06 11:42:59 +0800

    add more test

--- hash/hashmap_test.c
+++ hash/hashmap_test.c
@@ -17,8 +17,10 @@ int main(int argc, char **argv)
     int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 32; i++) {
         hashmap_insert(map, &arr[i], &arr[i]);
+        hashmap_dump(map, print_int);
+    }
     hashmap_dump(map, print_int);
 
     int key = 5;
@@ -35,6 +37,8 @@ int main(int argc, char **argv)
     val = hashmap_find(map, &key);
     print_int(val);
     printf("\n");
+
+    hashmap_destroy(map);
 
     return 0;
 }

7697307bf1c98943678343378119bc55f4cc4c6e
MeowDada <chang.hungjui1251@gmail.com>
2019-09-09 10:32:39 +0800

    Add more test

--- hash/hashmap_test.c
+++ hash/hashmap_test.c
@@ -17,6 +17,8 @@ int main(int argc, char **argv)
     int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 
+    int arr2[] = {50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 100, 101, 102, 103, 104};
+
     for (int i = 0; i < 32; i++)
         hashmap_insert(map, &arr[i], &arr[i]);
     hashmap_dump(map, print_int);
@@ -40,6 +42,9 @@ int main(int argc, char **argv)
         key = i;
         hashmap_remove(map, &key);
     }
+    for (int i = 0; i < 15; i++) 
+        hashmap_insert(map, &arr2[i], &arr2[i]);
+    
     hashmap_dump(map, print_int);
 
     hashmap_destroy(map);
